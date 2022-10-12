41526146590fc5e127bfa8649a5f1e4ae3565d4b
Игорь Кудрявцев <kudriavtsiev_2004@mail.ru>
2022-10-12 21:18:24 +0300

    add year check

---
+++ DateTime.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var year:=DateTime.Now.Year;
+  if year mod 4 = 0 then begin
+    if year mod 100 <> 0 then println('Високосный')
+    else if year mod 400 = 0 then println('Високосный')
+    else println('Не високосный');
+  end
+  else println('Не високосный');
+end.
